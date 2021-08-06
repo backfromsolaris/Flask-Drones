fba7e586c85c2dcef651529bbe438436c18fc36a
backfromsolaris <porraszach@gmail.com>
2021-08-06 11:38:12 -0500

    updated config back to previous SQLAlchemy syntax to link with Heroku.

--- config.py
+++ config.py
@@ -18,5 +18,5 @@ class Config:
     Eventually will use hidden variable items - but for now, we'll leave them exposed in config
     """
     SECRET_KEY = "Gandalf the Grey"
-    DEPLOY_DATABASE_URL = os.environ.get('DEPLOY_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # Decreases unnecessary output in terminal
